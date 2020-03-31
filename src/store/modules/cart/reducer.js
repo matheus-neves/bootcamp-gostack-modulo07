79b42aafec9c8cb29b063a6a6a5c12b0566d213f
Matheus Neves <matheus.neves@prodigious.com>
2020-03-31 17:58:11 -0300

    finish module redux

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,29 +4,15 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(
-          product => product.id === action.product.id
-        );
-
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        const { product } = action;
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft =>
         draft.filter(product => product.id !== action.id)
       );
 
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(
           product => product.id === action.id
